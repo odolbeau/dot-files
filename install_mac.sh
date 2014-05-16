63f45f5121ec79676ff397d76013edd860e9a7d7
Olivier Dolbeau <contact@odolbeau.fr>
2014-05-16 13:38:57 +0200

    Complete install.sh

--- install_mac.sh
+++ install_mac.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
+xcode-select --install
+
 mkdir -p ~/Library/LaunchAgents
-brew install git wget autoconf bash coreutils ack
+brew install git wget autoconf bash coreutils ack bash-completion ruby python watch
 brew tap homebrew/dupes
-brew tap josegonzalez/homebrew-php
+brew tap homebrew/php
 brew install php54 --with-fpm --with-imap --with-homebrew-openssl
-ln -sfv /usr/local/opt/php54/*.plist ~/Library/LaunchAgents
-brew install php54-intl php54-amqp php54-apc php54-mcrypt php54-memcached php54-redis php54-yaml composer node mysql nginx redis maven graphviz elasticsearch fswatch
-ln -sfv /usr/local/opt/rabbitmq/*.plist ~/Library/LaunchAgents
-launchctl load ~/Library/LaunchAgents/homebrew.mxcl.rabbitmq.plist
-ln -sfv /usr/local/opt/memcached/*.plist ~/Library/LaunchAgents
-launchctl load ~/Library/LaunchAgents/homebrew.mxcl.memcached.plist
+brew install php54-intl php54-amqp php54-apc php54-mcrypt php54-memcached php54-redis php54-yaml
+brew install composer node mysql nginx redis maven graphviz elasticsearch fswatch curl tree
 npm install -g grunt-cli
-ln -sfv /usr/local/opt/mysql/*.plist ~/Library/LaunchAgents
-launchctl load ~/Library/LaunchAgents/homebrew.mxcl.mysql.plist
+npm install -g bower
 rm /usr/local/Cellar/mysql/*/my.cnf
-ln -sfv /usr/local/opt/nginx/*.plist ~/Library/LaunchAgents
-launchctl load ~/Library/LaunchAgents/homebrew.mxcl.nginx.plist
-ln -sfv /usr/local/opt/redis/*.plist ~/Library/LaunchAgents
-launchctl load ~/Library/LaunchAgents/homebrew.mxcl.redis.plist
-ln -sfv /usr/local/opt/redis/*.plist ~/Library/LaunchAgents
-launchctl load ~/Library/LaunchAgents/homebrew.mxcl.redis.plist
-ln -sfv /usr/local/opt/elasticsearch/*.plist ~/Library/LaunchAgents
-launchctl load ~/Library/LaunchAgents/homebrew.mxcl.elasticsearch.plist
+
+# Launch all
+brew services start elasticsearch
+brew services start php54
+brew services start mysql
+brew services start redis
+brew services start memcached
+brew services start rabbitmq
+sudo brew services start nginx
 
 # Applications
 brew install vim --override-system-vi
@@ -30,12 +28,17 @@ brew install macvim
 brew linkapps
 brew tap phinze/cask
 brew install brew-cask
-brew cask install adium dropbox pandoc sequel-pro slack spectacle
+brew cask install adium dropbox launchrocket pandoc sequel-pro slack spectacle
 
 curl -L https://www.opscode.com/chef/install.sh | sudo bash
 
 # Others
+sudo gem update --system
+sudo gem pristine --all --only-executables
+sudo gem install jekyll kramdown bundler eventmachine thin foodcritic nokogiri hitimes nio4r json dep-selector-libgecode dep_selector
+sudo gem install json -v '1.7.7'
 #sudo gem install compass
+pip install json
 
 # Update PHP Conf
 echo "memory_limit = 2048M" >> /usr/local/etc/php/5.4/php.ini
